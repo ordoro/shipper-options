cb924a877e65629f6c8650e5e1747d653ec141fe
Seth York <york.seth@gmail.com>
2021-04-21 13:00:23 -0500

    set encoding - For somre reason I'm getting an ascii error when loading so try setting the encoding

--- shipper_options/options.py
+++ shipper_options/options.py
@@ -1,5 +1,5 @@
 import json
 from os import path
 
-_f = open(path.join(path.dirname(__file__), 'ShipperOptions.json'), 'r')
+_f = open(path.join(path.dirname(__file__), 'ShipperOptions.json'), 'r', encoding='utf8')
 shipper_options = json.load(_f)
